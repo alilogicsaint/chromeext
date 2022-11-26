ba47155be438fda8255c7e1378c0adbd98f7fbc2
alilogicsaint <alilogicsaint@gmail.com>
2022-11-26 18:38:17 +0500

    11/26/2022

--- main.js
+++ main.js
@@ -1,31 +1,157 @@
 
-var data =[]
+// var data =[]
  
-document.getElementById("btn").addEventListener("click", myFunction());
+// document.getElementById("btn").addEventListener("click", myFunction());
 
-function myFunction(){
+
+
+// function myFunction(){
     
-   document.getElementById("numberInput").addEventListener('change', doThing);
-   function doThing(){
-    data.push(this.value)
-    chrome.storage.local.set({"todo":data});
-    
-   }
+//    document.getElementById("numberInput").addEventListener('change', doThing);
+ 
+//    function doThing(){
+//     data.push(this.value)
+//     chrome.storage.local.set({"todo":[...data]}); 
+//     }
+
+
+// }
+
+
+
+//     chrome.storage.local.get("todo", function(data){
+//     const limap = data.todo.map((value, index) => {
+//         return `<li class="dflex listli" key=${index}>
+//         <input type="checkbox" unchecked @click="${this.changeStatus}" /> 
+//         <p class="li_text">${value}</p> 
+//           <div>
+//            <a id="delet" value="${index}"  ><i class="fa fa-trash-o" aria-hidden="true"></i></a> 
+//            <a id="delet" value="${index}" > <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> 
+//           <div> 
+//         </li>`
+        
+//       });
    
-}
+//     document.querySelector(".lists").innerHTML=limap.join('');
+
+// });
+
+// $(document).on("change","#numberInput",function(e) {
+//     data.push(e.target.value)
+// })
 
 
-chrome.storage.local.get("todo", function(data){
+
+var data =[]
+
+$(document).on("click",".btn",function(e) {
     
-    data.todo.forEach(getlist);
-
-function getlist(value){
-    var div=document.createElement("li"); 
-    document.querySelector(".lists").appendChild(div); 
-    div.innerText=value;
-  }
- 
+    var textval=document.getElementById("numberInput").value
+    data.push(textval)
+    chrome.storage.sync.set({"todo":[...data]});
+    chrome.storage.sync.get("todo", function(getdata){
+        const limap = getdata.todo.map((value, index) => {
+            return `<li class="dflex listli" key=${index}>
+            <input type="checkbox" unchecked @click="${this.changeStatus}" /> 
+            <p class="li_text">${value}</p> 
+              <div>
+               <a id="delet" value="${index}"  ><i class="fa fa-trash-o" aria-hidden="true"></i></a> 
+               <a id="delet" value="${index}" > <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> 
+              <div> 
+            </li>`
+          });
+      document.querySelector(".lists").innerHTML=limap.join(''); 
+    }) 
 })
+
+
+
+chrome.storage.sync.get("todo", function(getdata){
+         getall=_.uniq(data.concat(getdata))
+   const limap = getdata.todo.map((value, index) => {
+       return `<li class="dflex listli" key=${index}>
+       <input type="checkbox" unchecked @click="${this.changeStatus}" /> 
+       <p class="li_text">${value}</p> 
+         <div>
+          <a id="delet" value="${index}"  ><i class="fa fa-trash-o" aria-hidden="true"></i></a> 
+          <a id="delet" value="${index}" > <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> 
+         <div> 
+       </li>`
+     });
+ document.querySelector(".lists").innerHTML=limap.join(''); 
+});
+
+
+
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+//     console.log(changes)
+//     var todos =changes.todo
+//     // var data = todos.oldValue.concat(todos.newValue)
+   
+//     const limap = data.map((value, index) => {
+//         return `<li class="dflex listli" key=${index}>
+//         <input type="checkbox" unchecked @click="${this.changeStatus}" /> 
+//         <p class="li_text">${value}</p> 
+//           <div>
+//            <a id="delet" value="${index}"  ><i class="fa fa-trash-o" aria-hidden="true"></i></a> 
+//            <a id="delet" value="${index}" > <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> 
+//           <div> 
+//         </li>`
+//       });
+   
+//     document.querySelector(".lists").innerHTML=limap.join('');
+// });
+
+
+
+
+
+
+
+
+
+// document.getElementById("delet").addEventListener('change', removeitem)
+//         function removeitem(){ 
+//              console.log(this.value)
+//              chrome.storage.local.get("todos", function(data) {
+//                 var todos = data.todos;
+//                 todos.splice(i, 1);
+//                 chrome.storage.local.set({"todo":[...data]}); 
+//               });
+        
+//   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // chrome.storage.onChanged.addListener(
